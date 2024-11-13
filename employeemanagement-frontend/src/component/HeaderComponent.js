32b19778fa59b4d82b699d3d073ffb40240e5213
Kawin Raj <kawinraj1587@gmail.com>
2024-11-13 16:33:27 +0530

    second update

--- employeemanagement-frontend/src/component/HeaderComponent.js
+++ employeemanagement-frontend/src/component/HeaderComponent.js
@@ -1,17 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HeaderComponent = () => {
     return (
         <div>
             <header>
-                <nav className='navbar navbar-expand-md navbar-dark bg-dark p-3'>
+                <nav className="navbar navbar-expand-md navbar-dark bg-dark p-3">
                     <div>
-                        <a className='navbar-brand m-5' href="">Employee Management System</a>
+                        {/* Use Link for routing in React */}
+                        <Link className="navbar-brand m-5" to="/">
+                            Employee Management System
+                        </Link>
                     </div>
                 </nav>
             </header>
         </div>
-    )
-}
+    );
+};
 
-export default HeaderComponent
+export default HeaderComponent;
